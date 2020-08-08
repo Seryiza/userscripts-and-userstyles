0a6df6b2b87c44e226385e49499e5bfe750edd83
Sergey Zaborovsky <seryiza@yandex.ru>
2020-08-09 00:45:08 +1100

    Убрана функция-обертка за её избыточность

--- HotkeyHandler/hotkey-handler.user.js
+++ HotkeyHandler/hotkey-handler.user.js
@@ -4,55 +4,53 @@
 // @author Seryiza
 // @copyright 2020, Sergey Zaborovsky (seryiza.xyz)
 // @licence MIT
-// @version 1.0.1
+// @version 1.0.2
 // @exclude *
 // ==/UserScript==
 
-(() => {
-    const handlers = [];
-    const pressedCodes = new Set();
+const handlers = [];
+const pressedCodes = new Set();
 
-    function handleHotkey(keyCodes, handler) {
-        handlers.push({
-            handler,
-            keyCodes: new Set(keyCodes),
-        });
+function handleHotkey(keyCodes, handler) {
+    handlers.push({
+        handler,
+        keyCodes: new Set(keyCodes),
+    });
+}
+
+function areSetsEquals(set1, set2) {
+    if (set1.size !== set2.size) {
+        return false;
     }
 
-    function areSetsEquals(set1, set2) {
-        if (set1.size !== set2.size) {
+    for (let value of set1) {
+        if (!set2.has(value)) {
             return false;
         }
+    }
+    return true;
+}
 
-        for (let value of set1) {
-            if (!set2.has(value)) {
-                return false;
-            }
+document.addEventListener('keydown', (event) => {
+    const keyCode = event.code;
+    pressedCodes.add(keyCode);
+
+    let wasAnyHandlerCalled = false;
+    for (let { handler, keyCodes } of handlers) {
+        if (areSetsEquals(keyCodes, pressedCodes)) {
+            handler();
+            wasAnyHandlerCalled = true;
         }
-        return true;
     }
 
-    document.addEventListener('keydown', (event) => {
-        const keyCode = event.code;
-        pressedCodes.add(keyCode);
+    if (wasAnyHandlerCalled) {
+        pressedCodes.clear();
+    }
+});
 
-        let wasAnyHandlerCalled = false;
-        for (let {handler, keyCodes} of handlers) {
-            if (areSetsEquals(keyCodes, pressedCodes)) {
-                handler();
-                wasAnyHandlerCalled = true;
-            }
-        }
+document.addEventListener('keyup', (event) => {
+    const keyCode = event.code;
+    pressedCodes.delete(keyCode);
+});
 
-        if (wasAnyHandlerCalled) {
-            pressedCodes.clear();
-        }
-    });
-
-    document.addEventListener('keyup', (event) => {
-        const keyCode = event.code;
-        pressedCodes.delete(keyCode);
-    });
-
-    window.handleHotkey = handleHotkey;
-})()
+window.handleHotkey = handleHotkey;
