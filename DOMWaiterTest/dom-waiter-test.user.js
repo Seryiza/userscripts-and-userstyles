a56de2f9ca630992deb69ce3643c1253dd12f42e
Sergey Zaborovsky <seryiza@yandex.ru>
2021-01-26 23:01:04 +1100

    Добавлена декларания юзерскрипта для тестирования DOMWaiter

---
+++ DOMWaiterTest/dom-waiter-test.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name DOMWaiter Test
+// @copyright 2021, Seryiza (https://seryiza.xyz)
+// @version 2.0.0
+// @license MIT
+// @run-at document-end
+// @include *
+// @require https://github.com/Seryiza/userscripts-and-userstyles/raw/master/DOMWaiter/dom-waiter.js
+// @grant none
+// ==/UserScript==
